a8883607356676919b6a51e1502f2afac731dfed
Amanda Marques Furtado <amanda.furtado@ahrbrasil.com.br>
2022-06-29 20:25:19 -0300

    Decodificador de texto

---
+++ script.js
@@ -0,0 +1,38 @@
+const inputTexto = document.querySelector(".textareaTexto");
+const mensagem = document.querySelector(".textareaMensagem");
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(inputTexto.value)
+    mensagem.value = textoEncriptado
+    mensagem.style.backgroundImage = "none"
+}
+
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [["u", "ufat"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["a", "ai"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptada;
+}
+
+function btnDesencriptar() {
+    const textoDesencriptado = desencriptar(inputTexto.value)
+    mensagem.value = textoDesencriptado
+    mensagem.style.backgroundImage = "none"
+}
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [["ai", "a"], ["enter", "e"], ["imes", "i"], ["ober", "o"], ["ufat", "u"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptada.includes(matrizCodigo[i][0])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return stringDesencriptada;
+}
